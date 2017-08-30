07aaa023efe464b4380b100add0bcd426f5ea231
floris <floris@northpool.nl>
2017-08-30 09:57:14 +0200

    stuff

--- sif/target/xtend-gen/main/com/sirolf2009/yggdrasil/sif/transmutation/CSV.java
+++ sif/target/xtend-gen/main/com/sirolf2009/yggdrasil/sif/transmutation/CSV.java
@@ -14,33 +14,51 @@ import org.eclipse.xtext.xbase.lib.ListExtensions;
 public class CSV {
   private final static Logger log = LogManager.getLogger();
   
-  public static Function<List<List<Double>>, List<String>> matrixToCSV = ((Function<List<List<Double>>, List<String>>) (List<List<Double>> it) -> {
-    List<String> _xblockexpression = null;
-    {
-      CSV.log.info("Converting to csv");
-      final Function1<List<Double>, String> _function = (List<Double> it_1) -> {
-        final Function1<Double, String> _function_1 = (Double it_2) -> {
-          return (it_2 + "");
+  public static Function<List<List<Double>>, List<String>> matrixToCSV = new Function<List<List<Double>>, List<String>>() {
+    @Override
+    public List<String> apply(final List<List<Double>> it) {
+      List<String> _xblockexpression = null;
+      {
+        CSV.log.info("Converting to csv");
+        final Function1<List<Double>, String> _function = new Function1<List<Double>, String>() {
+          @Override
+          public String apply(final List<Double> it) {
+            final Function1<Double, String> _function = new Function1<Double, String>() {
+              @Override
+              public String apply(final Double it) {
+                return (it + "");
+              }
+            };
+            final Function2<String, String, String> _function_1 = new Function2<String, String, String>() {
+              @Override
+              public String apply(final String a, final String b) {
+                return ((a + ",") + b);
+              }
+            };
+            return IterableExtensions.<String>reduce(ListExtensions.<Double, String>map(it, _function), _function_1);
+          }
         };
-        final Function2<String, String, String> _function_2 = (String a, String b) -> {
-          return ((a + ",") + b);
-        };
-        return IterableExtensions.<String>reduce(ListExtensions.<Double, String>map(it_1, _function_1), _function_2);
-      };
-      _xblockexpression = ListExtensions.<List<Double>, String>map(it, _function);
+        _xblockexpression = ListExtensions.<List<Double>, String>map(it, _function);
+      }
+      return _xblockexpression;
     }
-    return _xblockexpression;
-  });
+  };
   
-  public static Function<List<String>, String> joinAsLines = ((Function<List<String>, String>) (List<String> it) -> {
-    String _xblockexpression = null;
-    {
-      CSV.log.info("Joining lines");
-      final BinaryOperator<String> _function = (String a, String b) -> {
-        return ((a + "\n") + b);
-      };
-      _xblockexpression = it.stream().reduce(_function).get();
+  public static Function<List<String>, String> joinAsLines = new Function<List<String>, String>() {
+    @Override
+    public String apply(final List<String> it) {
+      String _xblockexpression = null;
+      {
+        CSV.log.info("Joining lines");
+        final BinaryOperator<String> _function = new BinaryOperator<String>() {
+          @Override
+          public String apply(final String a, final String b) {
+            return ((a + "\n") + b);
+          }
+        };
+        _xblockexpression = it.stream().reduce(_function).get();
+      }
+      return _xblockexpression;
     }
-    return _xblockexpression;
-  });
+  };
 }
